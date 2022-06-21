8c042044c2243c57bf11eea6c77b316dfa254c71
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-06-21 12:49:35 -0400

    tailwind prettier plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,5 +5,5 @@ module.exports = {
   singleQuote: true,
   bracketSpacing: true,
   arrowParens: 'always',
-  tailwindConfig: './tailwind.config.js',
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
