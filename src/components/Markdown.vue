c24683e954ddaf0e9fa5cc7538c399eddfa6be88
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-06-21 16:10:07 -0400

    minor fixes

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="prose m-auto max-w-2xl text-left prose-a:text-blue-400 prose-blockquote:rounded prose-blockquote:bg-gray-50 prose-blockquote:py-1 dark:prose-h2:text-gray-200 dark:prose-h3:text-gray-200 dark:prose-p:text-gray-200 dark:prose-blockquote:bg-transparent dark:prose-strong:text-gray-200 dark:prose-code:text-gray-200 dark:prose-li:text-gray-200 dark:prose-th:text-gray-200 dark:prose-td:text-gray-200"
+    class="prose m-auto max-w-2xl text-left prose-a:text-blue-500 prose-blockquote:rounded prose-blockquote:bg-gray-50 prose-blockquote:py-1 dark:prose-h2:text-gray-200 dark:prose-h3:text-gray-200 dark:prose-p:text-gray-200 dark:prose-blockquote:bg-transparent dark:prose-strong:text-gray-200 dark:prose-code:text-gray-200 dark:prose-li:text-gray-200 dark:prose-th:text-gray-200 dark:prose-td:text-gray-200"
   >
     <slot />
   </div>
--- src/components/ui/BaseThemeToggle.vue
+++ src/components/ui/BaseThemeToggle.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { MoonIcon, SunIcon } from '@heroicons/vue/solid'
+import { SunIcon, MoonIcon } from '@heroicons/vue/outline'
+// import { MoonIcon } from '@heroicons/vue/solid'
 
 import { ref } from 'vue'
 
@@ -10,14 +11,14 @@ const enabled = ref(isDark)
   <Switch
     v-model="enabled"
     :class="[enabled ? 'bg-gray-600' : 'bg-gray-200']"
-    class="relative inline-flex h-7 w-12 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none"
+    class="relative inline-flex h-6 w-12 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none"
   >
     <span class="sr-only">Use setting</span>
     <span
       :class="[
         enabled ? 'translate-x-5 bg-gray-800' : 'translate-x-0 bg-white',
       ]"
-      class="pointer-events-none relative inline-block h-6 w-6 transform rounded-full shadow ring-0 transition duration-200 ease-in-out"
+      class="pointer-events-none relative inline-block h-5 w-5 transform rounded-full shadow ring-0 transition duration-200 ease-in-out"
     >
       <span
         :class="[
@@ -28,7 +29,7 @@ const enabled = ref(isDark)
         class="absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
         aria-hidden="true"
       >
-        <SunIcon class="h-5 text-gray-600" />
+        <SunIcon class="h-4 text-cyan-600" />
       </span>
       <span
         :class="[
@@ -39,7 +40,7 @@ const enabled = ref(isDark)
         class="absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
         aria-hidden="true"
       >
-        <MoonIcon class="h-5 text-white" />
+        <MoonIcon class="h-4 text-white" />
       </span>
     </span>
   </Switch>
