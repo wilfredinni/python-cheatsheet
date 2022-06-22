148d95ca80ac4035861d2172240fc7c178a32373
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-06-21 22:07:03 -0400

    navbar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -34,34 +34,50 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
                 alt="Workflow"
               />
             </div>
-            <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-              <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
+          </div>
+
+          <!-- actions -->
+          <div
+            class="absolute inset-y-0 right-0 flex items-center space-x-6 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+          >
+            <div
+              class="hidden border-r border-slate-200 pr-6 dark:border-slate-800 sm:ml-6 sm:flex sm:space-x-6"
+            >
               <a
                 href="#"
-                class="inline-flex items-center border-b-2 border-blue-500 px-1 pt-1 text-sm font-medium text-gray-900 dark:text-gray-200"
+                class="inline-flex items-center px-1 pt-1 text-sm font-medium text-sky-600 dark:text-sky-300"
               >
                 Cheatsheet
               </a>
               <a
                 href="#"
-                class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-200"
+                class="inline-flex items-center border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:text-sky-500 dark:text-gray-200 dark:hover:text-sky-400"
               >
                 Blog
               </a>
               <a
                 href="#"
-                class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 dark:text-gray-200"
+                class="inline-flex items-center border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:text-sky-500 dark:text-gray-200 dark:hover:text-sky-400"
               >
                 About
               </a>
             </div>
-          </div>
-
-          <!-- actions -->
-          <div
-            class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-          >
             <base-theme-toggle />
+            <a
+              target="_blank"
+              href="https://github.com/wilfredinni/python-cheatsheet/tree/next"
+            >
+              <svg
+                viewBox="0 0 16 16"
+                class="h-5 w-5 text-slate-500 hover:text-slate-600"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
