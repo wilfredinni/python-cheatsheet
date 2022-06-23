124c18afd59ba36160164e2271adeeda0be36b95
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-06-22 22:28:58 -0400

    space for sidebar

--- src/components/layout/TheSidebar.vue
+++ src/components/layout/TheSidebar.vue
@@ -100,24 +100,26 @@ const pages = [
 </script>
 
 <template>
-  <h5 class="mb-8 font-semibold text-slate-900 dark:text-slate-200 lg:mb-3">
-    Cheatsheet
-  </h5>
-  <ul
-    class="space-y-6 border-l border-slate-100 dark:border-slate-800 lg:space-y-2"
-  >
-    <li v-for="item in pages" :key="item.name">
-      <router-link
-        :to="item.path"
-        class="-ml-px block border-l border-transparent pl-4"
-        :class="
-          $route.path === item.path
-            ? 'border-current font-semibold text-sky-500 dark:text-sky-400'
-            : 'text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:border-slate-500 dark:hover:text-slate-300'
-        "
-      >
-        {{ item.name }}
-      </router-link>
-    </li>
-  </ul>
+  <div class="mt-5">
+    <h5 class="mb-8 font-semibold text-slate-900 dark:text-slate-200 lg:mb-3">
+      Cheatsheet
+    </h5>
+    <ul
+      class="space-y-6 border-l border-slate-100 dark:border-slate-800 lg:space-y-2"
+    >
+      <li v-for="item in pages" :key="item.name">
+        <router-link
+          :to="item.path"
+          class="-ml-px block border-l border-transparent pl-4"
+          :class="
+            $route.path === item.path
+              ? 'border-current font-semibold text-sky-500 dark:text-sky-400'
+              : 'text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:border-slate-500 dark:hover:text-slate-300'
+          "
+        >
+          {{ item.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
