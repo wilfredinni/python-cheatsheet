d201dace02db31406b6f478adc3dfdb4a49c5651
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-03 21:27:19 -0400

    style: blog list

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -19,31 +19,27 @@ const articles = computed(() => {
   <prose>
     <h1>Blog</h1>
     <p class="lead">
-      Basic cheatsheet for Python mostly based on the book
-      <a href="https://automatetheboringstuff.com/">
-        Automate the Boring Stuff with Python
-      </a>
-      and many other sources.
+      These articles provide a deeper look to the the contents of the
+      <router-link to="/">Python Cheatsheet</router-link>.
     </p>
-    <p class="lead"></p>
   </prose>
 
   <div class="mt-5">
-    <ul class="border-l border-l-slate-200 pl-2 dark:border-l-slate-800">
+    <ul class="border-l border-l-slate-200 dark:border-l-slate-800">
       <li
         v-for="article in articles"
         :key="article.path"
-        class="my-4 flex rounded-2xl px-6 py-5 hover:bg-sky-50 hover:dark:bg-slate-800/60 hover:dark:ring-1 hover:dark:ring-slate-300/10"
+        class="my-4 flex rounded-r-2xl px-6 py-5 hover:bg-sky-50 hover:dark:bg-slate-800/60 hover:dark:ring-1 hover:dark:ring-slate-300/10"
       >
         <router-link :to="article.path">
-          <span class="font-display text-sm font-semibold text-sky-500">
+          <span class="text-sm font-medium text-sky-500">
             Posted on {{ article.children[0]?.meta?.date }}
           </span>
-          <h2 class="text-lg font-semibold text-slate-700 dark:text-slate-300">
+          <h2 class="text-lg font-semibold text-slate-600 dark:text-slate-300">
             {{ article.children[0]?.meta?.title }}
           </h2>
 
-          <p class="text-slate-700 line-clamp-2 dark:text-slate-400">
+          <p class="text-slate-600 line-clamp-2 dark:text-slate-400">
             {{ article.children[0]?.meta?.description }}
           </p>
           <div class="mt-3 flex items-center text-sm font-medium text-sky-500">
