7cbf03074a4756d1e9cd7d00f8715e18fba747e3
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-03 21:52:57 -0400

    fix: spelling

--- src/components/ui/BaseReaderMode.vue
+++ src/components/ui/BaseReaderMode.vue
@@ -7,7 +7,7 @@ const core = useCoreStore()
     class="flex w-auto items-center rounded-full bg-sky-400/10 py-1 px-3 text-xs font-medium leading-5 text-sky-600 hover:bg-sky-400/20 dark:text-sky-400"
     @click="core.toggle()"
   >
-    <strong class="font-semibold"> Enter reading mode </strong>
+    <strong class="font-semibold"> Enter reader mode </strong>
     <svg
       width="3"
       height="6"
