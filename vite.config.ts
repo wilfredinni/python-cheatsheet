122687137d8c3300625af695bedc4222b6f9b4e6
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-04 21:21:20 -0400

    icons

--- vite.config.ts
+++ vite.config.ts
@@ -96,17 +96,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/snake.svg',
+            src: '/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/snake.svg',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/snake.svg',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
