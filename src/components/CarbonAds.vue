6dcfc3161232e768bdac043d9b6b67d386408faa
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-17 15:19:44 -0400

    feat: carbon env variables

--- src/components/CarbonAds.vue
+++ src/components/CarbonAds.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
 const container = ref()
 let hasInitalized = false
+const loadCarbon = import.meta.env.VITE_LOAD_CARBON || false
 function init() {
   if (!hasInitalized) {
+    const serve = import.meta.env.VITE_CARBON_SERVE
+    const placement = import.meta.env.VITE_CARBON_PLACEMENT
     hasInitalized = true
     const s = document.createElement('script')
     s.id = '_carbonads_js'
-    s.src = `https://cdn.carbonads.com/carbon.js?serve=CK7D62JL&placement=wwwpythoncheatsheetorg`
+    s.src = `https://cdn.carbonads.com/carbon.js?serve=${serve}&placement=${placement}`
     s.async = true
     container.value.appendChild(s)
   }
 }
 onMounted(() => {
-  init()
+  if (loadCarbon === 'true') {
+    init()
+  }
 })
 </script>
 
 <template>
-  <div id="carbonads" ref="container" class="mt-2" />
+  <div v-if="loadCarbon === 'true'" id="carbonads" ref="container" />
 </template>
 
 <style scoped>
