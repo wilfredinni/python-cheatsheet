5214b2a693f09e9ef1da0c61cf1b534b5c2aa5ed
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-18 23:20:30 -0400

    test: test sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import generateSitemap from 'vite-ssg-sitemap'
 import Pages from 'vite-plugin-pages'
@@ -13,127 +13,133 @@ import Prism from 'markdown-it-prism'
 import LinkAttributes from 'markdown-it-link-attributes'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
-
-  plugins: [
-    vue({
-      include: [/\.vue$/, /\.md$/],
-      reactivityTransform: true,
-    }),
-
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages({
-      extensions: ['vue', 'md'],
-      dirs: [
-        { dir: 'src/pages', baseRoute: '' },
-        { dir: 'cheatsheet', baseRoute: 'cheatsheet' },
-        { dir: 'blog', baseRoute: 'blog' },
-      ],
-    }),
-
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
-
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        '@vueuse/head',
-        '@vueuse/core',
-      ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/composables', 'src/store'],
-      vueTemplate: true,
-      eslintrc: {
-        enabled: true,
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+  return {
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
-    }),
+    },
 
-    // https://github.com/antfu/unplugin-vue-components
-    Components({
-      // allow auto load markdown components under `./src/components/`
-      dirs: ['src/components'],
-      extensions: ['vue', 'md'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts',
+    plugins: [
+      vue({
+        include: [/\.vue$/, /\.md$/],
+        reactivityTransform: true,
+      }),
 
-      resolvers: [HeadlessUiResolver()],
-    }),
+      // https://github.com/hannoeru/vite-plugin-pages
+      Pages({
+        extensions: ['vue', 'md'],
+        dirs: [
+          { dir: 'src/pages', baseRoute: '' },
+          { dir: 'cheatsheet', baseRoute: 'cheatsheet' },
+          { dir: 'blog', baseRoute: 'blog' },
+        ],
+      }),
 
-    // https://github.com/antfu/vite-plugin-md
-    Markdown({
-      headEnabled: true,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism, {})
-        md.use(LinkAttributes, {
-          matcher: (link: string) => /^https?:\/\//.test(link),
-          attrs: {
-            target: '_blank',
-            rel: 'noopener',
-          },
+      // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
+
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'vue-i18n',
+          '@vueuse/head',
+          '@vueuse/core',
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: ['src/composables', 'src/store'],
+        vueTemplate: true,
+        eslintrc: {
+          enabled: true,
+        },
+      }),
+
+      // https://github.com/antfu/unplugin-vue-components
+      Components({
+        // allow auto load markdown components under `./src/components/`
+        dirs: ['src/components'],
+        extensions: ['vue', 'md'],
+        // allow auto import and register components used in markdown
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: 'src/components.d.ts',
+
+        resolvers: [HeadlessUiResolver()],
+      }),
+
+      // https://github.com/antfu/vite-plugin-md
+      Markdown({
+        headEnabled: true,
+        markdownItSetup(md) {
+          // https://prismjs.com/
+          md.use(Prism, {})
+          md.use(LinkAttributes, {
+            matcher: (link: string) => /^https?:\/\//.test(link),
+            attrs: {
+              target: '_blank',
+              rel: 'noopener',
+            },
+          })
+        },
+      }),
+
+      // https://github.com/antfu/vite-plugin-pwa
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+        manifest: {
+          name: 'Python Cheatsheet',
+          short_name: 'Python Cheatsheet',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
+
+    // https://github.com/antfu/vite-ssg
+    ssgOptions: {
+      script: 'async',
+      formatting: 'minify',
+      // format: 'cjs',
+      onFinished() {
+        const hostname = `https://${process.env.VITE_BASE_URL}`
+        generateSitemap({
+          hostname,
         })
       },
-    }),
+    },
 
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: 'Python Cheatsheet',
-        short_name: 'Python Cheatsheet',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
+    // https://github.com/vitest-dev/vitest
+    test: {
+      environment: 'jsdom',
+      deps: {
+        inline: ['@vue', '@vueuse', 'vue-demi'],
       },
-    }),
-  ],
-
-  // https://github.com/antfu/vite-ssg
-  ssgOptions: {
-    script: 'async',
-    formatting: 'minify',
-    // format: 'cjs',
-    onFinished() {
-      generateSitemap()
     },
-  },
 
-  // https://github.com/vitest-dev/vitest
-  test: {
-    environment: 'jsdom',
-    deps: {
-      inline: ['@vue', '@vueuse', 'vue-demi'],
+    ssr: {
+      // TODO: workaround until they support native ESM
+      noExternal: ['workbox-window'],
     },
-  },
-
-  ssr: {
-    // TODO: workaround until they support native ESM
-    noExternal: ['workbox-window'],
-  },
+  }
 })
