3b3337b770552c887dbcd00c6d52be8de512206b
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-25 20:47:50 -0400

    chore: algolia key navigation

--- src/components/AlgoliaDocSearch.vue
+++ src/components/AlgoliaDocSearch.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 import docsearch from '@docsearch/js'
 
+const route = useRoute()
+const router = useRouter()
+
 onMounted(() => {
   docsearch({
     container: import.meta.env.VITE_DOCSEARCH_CONTAINER,
     appId: import.meta.env.VITE_DOCSEARCH_APP_ID,
     indexName: import.meta.env.VITE_DOCSEARCH_INDEX_NAME,
     apiKey: import.meta.env.VITE_DOCSEARCH_API_KEY,
+
+    navigator: {
+      navigate({ itemUrl }) {
+        const { pathname, hash } = new URL(itemUrl)
+        if (route.path === pathname) {
+          window.location.assign(pathname + hash)
+        } else {
+          router.push(pathname)
+        }
+      },
+    },
   })
 })
 </script>
