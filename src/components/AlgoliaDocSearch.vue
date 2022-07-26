7b5c87455acc93d85ec5db1f3d00b90e31be4b8c
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-07-25 21:59:44 -0400

    feat: docsearch click event

--- src/components/AlgoliaDocSearch.vue
+++ src/components/AlgoliaDocSearch.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 import docsearch from '@docsearch/js'
 
 const route = useRoute()
@@ -13,16 +13,62 @@ onMounted(() => {
 
     navigator: {
       navigate({ itemUrl }) {
-        const { pathname, hash } = new URL(itemUrl)
-        if (route.path === pathname) {
-          window.location.assign(pathname + hash)
-        } else {
-          router.push(pathname)
-        }
+        if (route.path === itemUrl) window.location.assign(itemUrl)
+        else router.push(itemUrl)
       },
+    },
+
+    transformItems(items) {
+      return items.map((item) => {
+        const { pathname, hash } = new URL(item.url)
+        return Object.assign({}, item, {
+          url: pathname + hash,
+        })
+      })
+    },
+
+    hitComponent({ hit, children }) {
+      return {
+        __v: null,
+        type: 'a',
+        ref: undefined,
+        constructor: undefined,
+        key: undefined,
+
+        props: {
+          href: hit.url,
+
+          onClick(event) {
+            if (isSpecialClick(event)) return
+
+            // we rely on the native link scrolling when user is already on
+            // the right anchor because Router doesn't support duplicated
+            // history entries.
+            if (route.path === hit.url) return
+
+            // if the hits goes to another page, we prevent the native link
+            // behavior to leverage the Router loading feature.
+            if (route.path !== hit.url) event.preventDefault()
+
+            router.push(hit.url)
+          },
+
+          children,
+        },
+      }
     },
   })
 })
+
+function isSpecialClick(event) {
+  return (
+    event.button === 1 ||
+    event.altKey ||
+    event.ctrlKey ||
+    event.metaKey ||
+    event.shiftKey
+  )
+}
 </script>
 
 <template>
