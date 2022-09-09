c725611f05bb2946f93aa68a5f435b0e0da5f343
Carlos <carlos.montecinos@ctsturismo.cl>
2022-09-09 09:49:39 -0400

    fix: test

--- src/components/layout/TheToc.vue
+++ src/components/layout/TheToc.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 const { theToc, currentSection } = useToc()
-const { reload } = useCarbon()
+// const { reload } = useCarbon()
 </script>
 
 <template>
   <nav aria-labelledby="on-this-page-title" class="w-56">
-    <carbon-ads v-if="!reload" class="mb-4" />
+    <!-- <carbon-ads v-if="!reload" class="mb-4" /> -->
+    <carbon-ads class="mb-4" />
     <h3 class="font-display text-sm font-medium text-slate-900 dark:text-white">
       On this page
     </h3>
