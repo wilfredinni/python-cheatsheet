0f92da31a660f1a871476ced25908b2decbf56ef
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-09-10 16:49:47 -0300

    style: ad box fixed height

--- src/components/layout/TheToc.vue
+++ src/components/layout/TheToc.vue
@@ -5,27 +5,34 @@ const { reload } = useCarbon()
 
 <template>
   <nav aria-labelledby="on-this-page-title" class="w-56">
-    <div class="h-[270px]">
-      <carbon-ads v-if="!reload" class="mb-4" />
+    <div
+      class="h-[280px] w-[152px] rounded-md border border-slate-200 bg-slate-50 dark:border-slate-700 dark:bg-slate-800"
+    >
+      <carbon-ads v-if="!reload" />
     </div>
-    <h3 class="font-display text-sm font-medium text-slate-900 dark:text-white">
-      On this page
-    </h3>
 
-    <ul class="mt-4 text-sm">
-      <li v-for="item in theToc" :key="item.id">
-        <a
-          :href="`#${item.id}`"
-          class="block py-1 font-medium transition duration-200"
-          :class="
-            currentSection === item.id
-              ? 'text-sky-500 '
-              : ' text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-300'
-          "
-        >
-          {{ item.header }}
-        </a>
-      </li>
-    </ul>
+    <div class="mt-4">
+      <h3
+        class="font-display text-sm font-medium text-slate-900 dark:text-white"
+      >
+        On this page
+      </h3>
+
+      <ul class="mt-4 text-sm">
+        <li v-for="item in theToc" :key="item.id">
+          <a
+            :href="`#${item.id}`"
+            class="block py-1 font-medium transition duration-200"
+            :class="
+              currentSection === item.id
+                ? 'text-sky-500 '
+                : ' text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-300'
+            "
+          >
+            {{ item.header }}
+          </a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
