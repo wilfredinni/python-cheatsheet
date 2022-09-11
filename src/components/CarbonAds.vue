b656a04c93b699e8eab7a0cad37d0fd6a4305bee
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-09-11 00:22:36 -0300

    style: ad transition

--- src/components/CarbonAds.vue
+++ src/components/CarbonAds.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 const container = ref()
-function init() {
+
+const placeholder = ref(false)
+const init = () => {
+  placeholder.value = true
   const serve = import.meta.env.VITE_CARBON_SERVE
   const placement = import.meta.env.VITE_CARBON_PLACEMENT
   const s = document.createElement('script')
@@ -8,7 +11,12 @@ function init() {
   s.src = `https://cdn.carbonads.com/carbon.js?serve=${serve}&placement=${placement}`
   s.async = true
   container.value.appendChild(s)
+  debouncedFn()
 }
+
+const debouncedFn = useDebounceFn(() => {
+  placeholder.value = false
+}, 100)
 
 const loadCarbon = import.meta.env.VITE_LOAD_CARBON || false
 
@@ -22,8 +30,28 @@ onMounted(() => {
 <template>
   <div
     v-if="loadCarbon === 'true'"
-    class="h-[280px] w-[152px] rounded-md border border-slate-200 bg-slate-50 dark:border-slate-700 dark:bg-slate-800"
+    class="relative h-[280px] w-[152px] rounded-md border border-slate-200 bg-slate-50 dark:border-slate-700 dark:bg-slate-800"
   >
     <div id="carbonads" ref="container" />
+
+    <Transition>
+      <div
+        v-show="placeholder"
+        class="absolute top-0 h-[278px] w-[150px] rounded-md bg-slate-50 dark:border-slate-700 dark:bg-slate-800"
+      />
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
