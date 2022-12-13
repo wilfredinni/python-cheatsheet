24a03a12a2eb021f961ec1361f9b30661268c486
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-12-12 22:31:05 -0300

    chore: update kinsta link

--- src/components/KinstaBanner.vue
+++ src/components/KinstaBanner.vue
@@ -3,11 +3,7 @@
 <template>
   <base-banner storage-key-name="issue-1-banner">
     <template #shortMsg>
-      <a
-        class="mx-auto"
-        href="https://kinsta.com/application-hosting/?utm_source=pythoncheatsheet&utm_medium=digital&utm_campaign=python-hosting&utm_content=banner"
-        target="_blank"
-      >
+      <a class="mx-auto" href="https://ggt.ink/PWTIPB9" target="_blank">
         <img
           src="/sponsors/kinsta/SVG/PythonCheatSheet_banner_kinsta_logo.svg"
           loading="lazy"
@@ -17,19 +13,17 @@
     </template>
     <template #message>
       <!-- logo -->
-      <img
-        class="hidden lg:flex"
-        src="/sponsors/kinsta/SVG/PythonCheatSheet_banner_kinsta_logo.svg"
-        loading="lazy"
-        alt="Deploys by Netlify"
-      />
+      <a class="mx-auto" href="https://ggt.ink/PWTIPB9" target="_blank">
+        <img
+          class="hidden lg:flex"
+          src="/sponsors/kinsta/SVG/PythonCheatSheet_banner_kinsta_logo.svg"
+          loading="lazy"
+          alt="Deploys by Netlify"
+        />
+      </a>
 
       <!-- md: msg with link -->
-      <a
-        class="mx-auto"
-        href="https://kinsta.com/application-hosting/?utm_source=pythoncheatsheet&utm_medium=digital&utm_campaign=python-hosting&utm_content=banner"
-        target="_blank"
-      >
+      <a class="mx-auto" href="https://ggt.ink/PWTIPB9" target="_blank">
         <img
           class="mx-auto hidden lg:block"
           src="/sponsors/kinsta/SVG/PythonCheatSheet_banner_Text.svg"
@@ -47,10 +41,7 @@
       />
 
       <!-- banner action -->
-      <a
-        target="_blank"
-        href="https://kinsta.com/application-hosting/?utm_source=pythoncheatsheet&utm_medium=digital&utm_campaign=python-hosting&utm_content=banner"
-      >
+      <a target="_blank" href="https://ggt.ink/PWTIPB9">
         <img
           class="hidden lg:flex"
           src="/sponsors/kinsta/SVG/PythonCheatSheet_banner_button-main.svg"
