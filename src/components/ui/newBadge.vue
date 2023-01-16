846b55d7c09e4ba1998815644cae7a2f9a91b13e
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2023-01-16 19:52:43 -0300

    style: new badge

--- src/components/ui/newBadge.vue
+++ src/components/ui/newBadge.vue
@@ -1,6 +1,7 @@
 <template>
   <span
-    class="rounded bg-orange-400 py-0.5 px-1 font-mono text-[0.60rem] tracking-widest text-white dark:bg-amber-500"
-    >NEW</span
+    class="rounded bg-orange-400 py-0.5 px-1 font-mono text-[0.60rem] tracking-widest text-white dark:bg-amber-600"
+  >
+    NEW</span
   >
 </template>
