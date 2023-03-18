bc0ef7cb4d2ad38e2bef3bb2c590aaab1946c3cc
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2023-03-18 13:45:22 -0300

    minor fix

--- src/modules/analytics.ts
+++ src/modules/analytics.ts
@@ -3,7 +3,6 @@ import { type UserModule } from '~/types'
 
 export const install: UserModule = ({ app }) => {
   const inProduction = import.meta.env.VITE_IN_PRODUCTION
-  console.log('inProduction', inProduction)
   if (inProduction) {
     const gTag = import.meta.env.VITE_GTAG || 'tag'
     app.use(VueGtag, {
