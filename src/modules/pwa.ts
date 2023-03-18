fde6388be15125457eb79653ceea70724dd913bd
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2023-03-18 13:54:23 -0300

    test: pwa workbox logs

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -4,8 +4,11 @@ import { type UserModule } from '~/types'
 export const install: UserModule = ({ isClient, router }) => {
   if (!isClient) return
 
-  router.isReady().then(async () => {
-    const { registerSW } = await import('virtual:pwa-register')
-    registerSW({ immediate: true })
-  })
+  router
+    .isReady()
+    .then(async () => {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({ immediate: true })
+    })
+    .catch(() => {})
 }
