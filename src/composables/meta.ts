6c00878448e598dcc8bf175dac6091a9d78ba4a7
Carlos <carlos@pythoncheatsheet.org>
2023-05-01 18:17:00 -0400

    update social banner

--- src/composables/meta.ts
+++ src/composables/meta.ts
@@ -4,7 +4,8 @@ export function useMeta() {
 
   const description =
     'Anyone can forget how to make character classes for a regex, slice a list or do a for loop. This cheat sheet tries to provide a basic reference for beginner and advanced developers, lower the entry barrier for newcomers and help veterans refresh the old tricks.'
-  const cardImage = 'public/social/social_banner.png'
+  const cardImage =
+    'https://raw.githubusercontent.com/wilfredinni/python-cheatsheet/master/public/social/social_banner.png'
   const themeColor = computed(() => (isDark.value ? '#1f2937' : '#ffffff'))
   const url = computed(() => `https://${base_url}${route.path}`)
   const keywords = [
