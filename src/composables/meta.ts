c43eaa28d8024c1d2b72dcdd4f75d393d3c02669
Carlos <carlos@pythoncheatsheet.org>
2023-05-01 18:08:52 -0400

    update banner

--- src/composables/meta.ts
+++ src/composables/meta.ts
@@ -4,8 +4,7 @@ export function useMeta() {
 
   const description =
     'Anyone can forget how to make character classes for a regex, slice a list or do a for loop. This cheat sheet tries to provide a basic reference for beginner and advanced developers, lower the entry barrier for newcomers and help veterans refresh the old tricks.'
-  const cardImage =
-    'https://raw.githubusercontent.com/wilfredinni/pysheetBlog/master/img/logo.png'
+  const cardImage = 'public/social/social_banner.png'
   const themeColor = computed(() => (isDark.value ? '#1f2937' : '#ffffff'))
   const url = computed(() => `https://${base_url}${route.path}`)
   const keywords = [
