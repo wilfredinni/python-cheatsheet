379679a1179544f3b3e4c04ae217642f18ab06c8
Carlos <carlos@pythoncheatsheet.org>
2023-07-16 21:17:24 -0400

    fix: prettier

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: false,
-  useTabs: false,
-  endOfLine: 'auto',
-  singleQuote: true,
-  bracketSpacing: true,
-  arrowParens: 'always',
-  plugins: [require('prettier-plugin-tailwindcss')],
-}
