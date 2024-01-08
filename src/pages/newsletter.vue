b1f76a38a3ffbffecf7b9fb983977697762986d7
Carlos <carlos@pythoncheatsheet.org>
2024-01-08 19:30:32 -0300

    Update Python developer count in SubscriptionForm and newsletter.vue

--- src/pages/newsletter.vue
+++ src/pages/newsletter.vue
@@ -76,7 +76,7 @@ const response = computed(() => newsletter.getResponse)
         <p class="mt-6 text-lg leading-8 text-slate-600 dark:text-slate-400">
           Join
           <span class="text-sky-600 dark:text-sky-500"
-            >9.800+ Python developers</span
+            >10.100+ Python developers</span
           >
           in a weekly and bullshit free publication, full of interesting,
           relevant links.
