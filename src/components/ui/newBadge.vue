9d53440c6630d34c7194560928d8a7a676174f8a
Carlos <carlos@pythoncheatsheet.org>
2024-01-18 11:15:00 -0300

    Update UI and navigation store

--- src/components/ui/newBadge.vue
+++ src/components/ui/newBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="rounded bg-orange-400 px-1 py-0.5 font-mono text-[0.60rem] tracking-widest text-white dark:bg-amber-600"
-    >NEW</span
+    class="rounded bg-orange-400 px-1 py-0.5 text-[0.60rem] tracking-widest text-white dark:bg-amber-600"
+    >Updated</span
   >
 </template>
