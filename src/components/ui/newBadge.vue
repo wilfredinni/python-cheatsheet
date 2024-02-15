64f747dd4b4ef644a42bb1d636f9efbc06fc39c2
Carlos <carlos@pythoncheatsheet.org>
2024-02-15 20:16:11 -0300

    update new badge style

--- src/components/ui/newBadge.vue
+++ src/components/ui/newBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="rounded bg-orange-400 px-1 py-0.5 text-[0.60rem] tracking-widest text-white dark:bg-amber-600"
+    class="rounded bg-amber-400 px-1 py-0.5 text-[0.60rem] tracking-widest dark:bg-amber-500 text-amber-950 font-bold"
     >New</span
   >
 </template>
